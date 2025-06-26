6f644d5d0a0653cf2f5a7226973f38bb8eca5dc8
Ralph <rorozco@salvi.es>
2025-06-26 17:27:45 +0200

    Update fscoverage.py

--- fscoverage.py
+++ fscoverage.py
@@ -312,7 +312,7 @@ if not geo_points.empty:
     init_view_state = pdk.ViewState(
         latitude=geo_points["lat"].mean(),
         longitude=geo_points["lon"].mean(),
-        zoom=40,
+        zoom=25,
     )
 else:
     init_view_state = pdk.ViewState(latitude=0, longitude=0, zoom=2)
